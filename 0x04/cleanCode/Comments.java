0383c4f914df9ea98c459c4c64e708fa498d29ab
Cristiano <cristiano@vbsolutions.com.br>
2025-08-02 09:43:00 -0300

    bradesco-hbtn-SOLID - Comentarios

---
+++ 0x04/cleanCode/Comments.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+import java.util.List;
+
+public class Comments {
+
+    // TODO: Este método deve ser removido
+    public void check() {
+        return;
+    }
+
+    public Owner getTestcaseOwner(Testcase testcase) {
+        // lógica de negócios
+        return null;
+    }
+
+    public Hours getHoursInQuarter() {
+        // lógica de negócios
+        return null;
+    }
+
+    /**
+     * Publica resultados com base na data fornecida.
+     *
+     * @param date Data a partir da qual os resultados devem ser publicados
+     * @return Lista de entidades de resultado
+     * @since 16.0
+     */
+    public List<Result> publishResults(Date date) {
+        // lógica de negócios
+        return null;
+    }
+
+    // Classes auxiliares simuladas
+    public static class Owner {}
+    public static class Testcase {}
+    public static class Hours {}
+    public static class Result {}
+}
